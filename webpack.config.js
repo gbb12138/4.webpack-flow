d17110068c5b715556c8be89fe2910969a7922bb
guobeibei <guobeibei@tal.com>
2021-12-23 18:22:34 +0800

    配置webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    devtool: false,
+    entry: {
+        entry1: './src/entry1.js',
+        entry2: './src/entry2.js'
+    }
+}
